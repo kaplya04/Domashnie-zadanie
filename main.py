0ee2c25fcd623c4f882d94b71f6d9779e62f52f0
Данил <101656626+spamli@users.noreply.github.com>
2023-09-05 20:47:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-a = input('Введите значение a: ')
-b = input('Введите значение b: ')
-c = input('Введите значение c: ')
-a = float(a)
-b = float(b)
-c = float(c)
-dis = b**2 - 4*a*c
-print('Дискриминант = ' + str(dis))
-if dis < 0:
-    print('Корней нет')
-elif dis == 0:
-    x = -b / (2 * a)
-    print('x = ' + str(x))
-else:
-    x1 = (-b + dis ** 0.5) / (2 * a)
-    x2 = (-b - dis ** 0.5) / (2 * a)
-    print('x1 = ' + str(x1))
-    print('x2 = ' + str(x2))
+chis = int(input("Введите число: "))
+st = input("Введите строку: ")
+mass = []
+for e in range(1,chis):
+    if e % 2 == 0:
+        e = st
+        mass.append(st)
+    else:mass.append(e)
+print(mass)
